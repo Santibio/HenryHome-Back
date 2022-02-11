494c8516682ff8c787ee367732df71c3f294d89d
Pedro <pedroagustinpeyon@gmail.com>
2022-02-11 11:24:58 -0300

    Patch Housing fix (Ya no son obligatorias todos atributos), readme update

--- src/routes/reservation.route.js
+++ src/routes/reservation.route.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const { createReservation , updateReservation } = require('../controllers/reservation.controller')
 const { veryfyToken, isModerador, isAdmin } = require("../middleware/auth.js");
+
 router.post('/',veryfyToken,createReservation)
 router.put('/',updateReservation)
 
